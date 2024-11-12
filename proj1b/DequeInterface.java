e1500ee9c07740ea0f8f81793165989c2105c24a
KleMoretti <niuniuniudechao@outlook.com>
2024-11-12 16:59:06 +0800

    :construction: study doing

---
+++ proj1b/DequeInterface.java
@@ -0,0 +1,15 @@
+import java.util.concurrent.LinkedBlockingDeque;
+
+public interface DequeInterface <T>{
+    public void addFirst(T item);
+    public void addLast(T item);
+    public default boolean isEmpty(){
+        return size() == 0;
+    }
+    public int size();
+    public void printDeque();
+    public T removeFirst();
+    public T removeLast();
+    public T get(int index);
+
+}
